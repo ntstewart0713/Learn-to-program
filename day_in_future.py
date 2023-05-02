a5e5d5680fff5f466f9ce9ef0952484e0c9a7eeb
ntstewart0713 <ntstewart0713@gmail.com>
2023-05-02 13:00:43 -0700

    Add files via upload

---
+++ day_in_future.py
@@ -0,0 +1,25 @@
+def get_weekday(current_weekday, days_ahead):
+    """ (int, int) -> int
+
+    Return which day of the week it will be days_ahead days from current_weekday.
+
+    current_weekday is the current day of the week and is in the range 1-7, indi    cating whether today is Sunday (1), Monday (2), ..., Saturday (7).
+
+    days_ahead is the number of days after today.
+
+    >>> get_weekday(3, 1)
+    4
+    >>> get_weekday(6, 1)
+    7
+    >>> get_weekday(7, 1)
+    1
+    >>> get_weekday(1, 0)
+    1
+    >>> get_weekday(4, 7)
+    4
+    >>> get_weekday(7, 72)
+    2
+    """
+    return current_weekday + days_ahead % 7
+
+get_weekday(3, 1)
---
+++ quadratic.pi.py
@@ -0,0 +1,7 @@
+def quadratic(a, b, c, x):
+    first = a * x ** 2
+    second = b * x
+    third = c
+    return first + second + third
+
+quadratic(2, 3, 4, 0.5)
