d19eec4c38d8a2a3a1df61d7da4e48046ec12443
ntstewart0713 <ntstewart0713@gmail.com>
2023-05-02 13:08:45 -0700

    Add files via upload

--- day_in_future.py
+++ day_in_future.py
@@ -20,6 +20,6 @@ def get_weekday(current_weekday, days_ahead):
     >>> get_weekday(7, 72)
     2
     """
-    return (current_weekday + days_ahead) % 7
+    return (current_weekday + days_ahead - 1) % 7 + 1
 
 get_weekday(3, 1)
