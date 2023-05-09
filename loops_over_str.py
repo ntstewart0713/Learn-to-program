40dde9054dc3dd239d7b9b95e9fe5098d52a9797
ntstewart0713 <ntstewart0713@gmail.com>
2023-05-09 14:58:32 -0700

    Add files via upload

--- loops_over_str.py
+++ loops_over_str.py
@@ -1,3 +1,23 @@
+def collect_vowels(s):
+    ''' (str) -> str
+
+    Return the vowels from s. Do not treat the letter y
+    as a vowel.
+    
+    >>> collect_vowels('Happy Anniversary!')
+    'aAiea'
+    >>> collect_vowels('xyz')
+    ''
+    '''
+
+    vowels = ''
+
+    for char in s:
+        if char in 'aeiouAEIOU':
+            vowels = vowels + char
+
+    return vowels
+    
 def count_vowels(s):
     ''' (str) -> int
 
